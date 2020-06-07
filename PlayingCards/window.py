7efd8628b444854286d90bd161af993a79888bba
KanaeOhta <kanae5321@gmail.com>
2020-06-07 23:28:05 +0900

    Modified to be able to switch game flames

--- PlayingCards/window.py
+++ PlayingCards/window.py
@@ -2,6 +2,7 @@ import os
 import tkinter as tk
 import tkinter.ttk as ttk
 
+import fourleafclover
 import pyramid
 from globals import *
 
@@ -16,6 +17,7 @@ class Window(ttk.Frame):
 
 
     def create_variables(self):
+        self.games = {}
         self.images = {}
         self.status_text = tk.StringVar()
 
@@ -23,7 +25,7 @@ class Window(ttk.Frame):
     def create_images(self):
         image_path = os.path.join(os.path.dirname(
             os.path.realpath(__file__)), IMAGE_ROOT)
-        for name in (CLOSE, PYRAMID, PIN, BACK, RELOAD):
+        for name in (CLOSE, PYRAMID, PIN, BACK, RELOAD, CLOVER, RULES):
             self.images[name] = tk.PhotoImage(
                 file=os.path.join(image_path, '{}.png'.format(name)))
 
@@ -32,11 +34,27 @@ class Window(ttk.Frame):
         self.create_board()
         self.create_menubar()
         self.create_statusbar()
+        self.change_board(PYRAMID)
 
 
     def create_board(self):
-        self.board = pyramid.Board(self.master, self.status_text, self.images[BACK], self.images[PIN])
-        self.board.pack(fill=tk.BOTH, expand=True)
+        container = tk.Frame(self.master)
+        container.pack(fill=tk.BOTH, expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        pyramid_frame = tk.Frame(container)
+        pyramid_frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+        # pyramid_board = pyramid.Board(pyramid_frame, self.status_text, self.images[BACK], self.images[PIN])
+        pyramid_board = pyramid.Board(pyramid_frame, self.status_text)
+        pyramid_board.pack(fill=tk.BOTH, expand=True)
+        self.games[PYRAMID] = (pyramid_frame, pyramid_board)
+
+        clover_frame = tk.Frame(container)
+        clover_frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+        clover_board = fourleafclover.Board(clover_frame, self.status_text)
+        clover_board.pack(fill=tk.BOTH, expand=True)
+        self.games[CLOVER] = (clover_frame, clover_board)
 
 
     def create_menubar(self):
@@ -48,29 +66,42 @@ class Window(ttk.Frame):
 
     def create_game_menu(self):
         gamemenu = tk.Menu(self.menubar, tearoff=0, name='game')
-        gamemenu.add_command(label=PYRAMID, command=self.board.new_game,
+        gamemenu.add_command(label=PYRAMID, command=lambda: self.change_board(PYRAMID),
             compound=tk.LEFT, image=self.images[PYRAMID])
-        gamemenu.add_command(label=CLOSE, command='',
+        gamemenu.add_command(label=CLOVER, command=lambda: self.change_board(CLOVER),
+            compound=tk.LEFT, image=self.images[CLOVER])
+        gamemenu.add_command(label=CLOSE, command=self.close,
             compound=tk.LEFT, image=self.images[CLOSE])
         self.menubar.add_cascade(label="Game", menu=gamemenu)
 
 
     def create_help_menu(self):
         helpmenu = tk.Menu(self.menubar, tearoff=0, name='help')
-        helpmenu.add_command(label=PYRAMID, command='',
-            compound=tk.LEFT, image=self.images[PYRAMID])
+        helpmenu.add_command(label=RULES, command='',
+            compound=tk.LEFT, image=self.images[RULES])
         self.menubar.add_cascade(label="Help", menu=helpmenu)
 
 
     def create_statusbar(self):
         statusbar = ttk.Frame(self.master)
-        repeat_btn = ttk.Button(statusbar, image=self.images[RELOAD], width=5, command=self.board.new_game)
+        repeat_btn = ttk.Button(statusbar, image=self.images[RELOAD], width=5, command=self.new_game)
         repeat_btn.pack(side=tk.RIGHT, pady=PAD)
         status_label = ttk.Label(statusbar, textvariable=self.status_text, font=('', 20))
         status_label.pack(side=tk.LEFT)
         statusbar.columnconfigure(0, weight=1)
+        # statusbar.grid(row=1, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
         statusbar.pack(side=tk.BOTTOM, fill=tk.X)
         
+
+    def change_board(self, board_name):
+        flame, board = self.games[board_name]
+        flame.tkraise()
+        self.board = board
+        self.new_game()
+
+
+    def new_game(self):
+        self.board.new_game()
 
 
     def close(self, event=None):
