4007a679dc5cef79136d7414b3b77768bb9b6627
KanaeOhta <kanae5321@gmail.com>
2020-06-07 23:27:03 +0900

    Modified Card class

--- PlayingCards/pyramid.py
+++ PlayingCards/pyramid.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+
 import os
 import random
 import tkinter as tk
@@ -20,99 +22,119 @@ DISCARD_Y = STOCK_Y
 STATUS_MESSAGE = 'No cards selected.'
 
 
+CardFace = namedtuple('CardFace', 'image mark value')
+
+
 class Card:
 
-    def __init__(self, item_id, face, status, x, y, left=None, right=None):
+    def __init__(self, item_id, face, status, x, y, face_up=False, left=None, right=None):
         self.id = item_id
         self.face = face 
         self.status = status
         self.x = x
         self.y = y
+        self.face_up = face_up
         self.left = left
         self.right = right
+        self.face_up = face_up
+        self.dele = False
         self.pin = None
 
 
-class CardFace:
+    @property
+    def value(self):
+        return self.face.value
 
-    def __init__(self, img, value, up=False, dele=False):
-        self.img = img 
-        self.value = value
-        self.up = up
-        self.dele = dele
+    
+    @property
+    def mark(self):
+        return self.face.mark
+
+
+    @property
+    def image(self):
+        return self.face.image
+
 
 
 class Board(tk.Canvas):
 
-    def __init__(self, master, status_text, back, pin, delay=400, rows=7):
+    def __init__(self, master, status_text, delay=400, rows=7):
+    # def __init__(self, master, status_text, back, pin, delay=400, rows=7):
         self.status_text = status_text
         self.rows = rows
         self.delay = delay
         self.stock = None
         self.discard_x = DISCARD_X
         self.discard_y = DISCARD_Y
-        self.opened = []
-        self.deck = [card for card in os.listdir(ROOT)]
-        self.back = back
-        self.pin = pin
+        self.selected = []
+        self.deck = [face for face in self.create_card()]
+        self.back = tk.PhotoImage(file='images/back.png')
+        self.pin = tk.PhotoImage(file='images/pin.png')
+        # self.back = back
+        # self.pin = pin
         self.playing_cards = {} 
         super().__init__(master, width=BOARD_WIDTH, height=BOARD_HEIGHT, bg=BOARD_COLOR)
         self.pack(fill=tk.BOTH, expand=True)
         self.new_game()
       
 
+    def create_card(self):
+        image_path = os.path.join(os.path.dirname(
+            os.path.realpath(__file__)), CARD_ROOT)
+        for path in os.listdir(image_path):
+            name = os.path.splitext(path)[0]
+            mark, value = name.split('_')    
+            yield CardFace(tk.PhotoImage(file=os.path.join(image_path, path)), mark, int(value))
+
+
     def new_game(self):
         self.delete('all')
         # config() changes attributes after creating object. 
         self.config(width=BOARD_WIDTH, height=BOARD_HEIGHT)
         random.shuffle(self.deck)
-
-        cards = [face for face in self.card_face(lambda x: not x.startswith('jocker'))]
-        jockers = [face for face in self.card_face(lambda x: x.startswith('jocker'))]
-
-        self.setup_pyramid(cards)
-        self.setup_stock(cards)
+        cards = [face for face in self.deck if not face.mark.startswith('jocker')]
+        jockers = [face for face in self.deck if face.mark.startswith('jocker')]    
+        limit = int(self.rows * (self.rows+1) / 2) # the number of pyramit cards
+        self.setup_pyramid(cards[:limit])
+        self.setup_stock(cards[limit:])
         self.setup_jocker(jockers)
-
         for name in self.playing_cards.keys():
             self.tag_bind(name, '<ButtonPress-1>', self.click)
 
 
-    def card_face(self, func):
-        image_path = os.path.join(os.path.dirname(
-            os.path.realpath(__file__)), CARD_ROOT)
-        for card in self.deck:
-            if func(card):
-                name = os.path.splitext(card)[0]
-                _, value = name.split('_')   
-                yield CardFace(tk.PhotoImage(file=os.path.join(image_path, card)), int(value))
-
-
-    def setup_pyramid(self, cards):
+    def setup_pyramid(self, pyramid_cards):
         start = 0
-        x, y = PYRAMID_X, PYRAMID_Y
+        cards = []
         for i in range(1, self.rows+1):
-            for face in cards[start: start + i]: 
-                index = cards.index(face)
-                name = 'pyramid{}'.format(index)
-                image = face.img if i == self.rows else self.back
-                # x, y are the center of the image
-                item_id = self.create_image(x, y, image=image, tags=name)
-                face.up = True if i == self.rows else False
-                left = cards[index + i] if i < self.rows else None
-                right = cards[index + i + 1] if i < self.rows else None
-                card = Card(item_id, face, 'pyramid', x, y, left, right)
+            cards.append(pyramid_cards[start:start + i])
+            start = start + i
+
+        x, y = PYRAMID_X, PYRAMID_Y
+        for i, row in enumerate(cards, 1):
+            for j, face in enumerate(row, 1):
+                name = 'pyramid{}{}'.format(i, j)
+                item_id = self.create_image(
+                    x, y,
+                    image=face.image if i == self.rows else self.back, 
+                    tags=name
+                )
+                face_up = True if i == self.rows else False
+                card = Card(item_id, face, 'pyramid', x, y, face_up)
                 self.playing_cards[name] = card
                 x += SPACE
-            start = start + i
             x -= (SPACE * i) + 45
             y += 40
-
+        for i, row in enumerate(cards[:-1], 1):
+            for j, _ in enumerate(row, 1):
+                card = self.playing_cards['pyramid{}{}'.format(i, j)]
+                card.left = self.playing_cards['pyramid{}{}'.format(i+1, j)]
+                card.right = self.playing_cards['pyramid{}{}'.format(i+1, j+1)]
+              
 
     def setup_stock(self, cards):
-        start = int(self.rows * (self.rows+1) / 2)
         x, y = STOCK_X, STOCK_Y
-        for i, face in enumerate(cards[start:]):
+        for i, face in enumerate(cards):
             name = 'stock{}'.format(i)
             item_id = self.create_image(x, y, image=self.back, tags=name)
             card = Card(item_id, face, 'stock', x, y)
@@ -125,23 +147,22 @@ class Board(tk.Canvas):
         x, y = JOCKER_X, JOCKER_Y
         for i, face in enumerate(jockers):
             name = 'jocker{}'.format(i)
-            item_id = self.create_image(x, y, image=face.img, tags=name)
-            face.up = True
-            card = Card(item_id, face, 'jocker', x, y)
+            item_id = self.create_image(x, y, image=face.image, tags=name)
+            card = Card(item_id, face, 'jocker', x, y, True)
             self.playing_cards[name] = card
             x += SPACE
 
 
     def click(self, event):
         card = self.get_card(event)
-        if card.status == 'stock' and card.face.up == False:
+        if card.status == 'stock' and card.face_up == False:
             if self.stock is None:
                 self.stock_face_up(card)
             else:
                 self.discard_stock()
                 self.stock_face_up(card)
             self.stock = card
-        elif card.face.up:
+        elif card.face_up:
             if not card.pin:
                 self.set_pin(card)
             self.judge(card)
@@ -154,10 +175,10 @@ class Board(tk.Canvas):
        
 
     def stock_face_up(self, card):
-        self.itemconfig(card.id, image=card.face.img)
+        self.itemconfig(card.id, image=card.image)
         self.coords(card.id, OPEN_STOCK_X, OPEN_STOCK_Y)
         card.x, card.y = OPEN_STOCK_X, OPEN_STOCK_Y 
-        card.face.up = True 
+        card.face_up = True 
 
 
     def discard_stock(self):
@@ -166,15 +187,15 @@ class Board(tk.Canvas):
         self.stock.x, self.stocky = self.discard_x, self.discard_y 
         self.discard_x += STACK_OFFSET
         self.discard_y -= STACK_OFFSET
-        self.stock.kind = 'discarded'
+        self.stock.status = 'discarded'
 
 
     def pyramid_face_up(self):
         cards = filter(lambda x: x.right and x.left, self.playing_cards.values())
         for card in cards:
             if card.right.dele and card.left.dele:
-                self.itemconfig(card.id, image=card.face.img)
-                card.face.up = True
+                self.itemconfig(card.id, image=card.image)
+                card.face_up = True
 
 
     def set_pin(self, card):
@@ -184,47 +205,47 @@ class Board(tk.Canvas):
 
     def judge(self, card):
         self.update_status(card)
-        if card.face.value == 13:
-            self.after(self.delay, lambda: self.delete_cards(card))
-        elif len(self.opened) == 1 and self.opened[0] == card:
-            self.remove_pins(card)
-            self.opened = []
+        if card.value == 13:
+            self.after(self.delay, lambda: self.delete_cards((card,)))
+            self.after(self.delay, self.pyramid_face_up)
+        elif len(self.selected) == 1 and self.selected[0] == card:
+            self.remove_pins((card,))
+            self.selected = []
         else:
-            self.opened.append(card)
-            if len(self.opened) == 2:
-                card1, card2 = self.opened[0], self.opened[1]
-                if card1.face.value + card2.face.value == 13 or \
-                         (card1.status == 'jocker' or card2.status == 'jocker'):
-                    self.after(self.delay, lambda: self.delete_cards(card1, card2))
+            self.selected.append(card)
+            if len(self.selected) == 2:
+                cards = self.selected[0:]
+                if sum(card.value == 14 for card in self.selected) >= 1 or \
+                        sum(card.value for card in self.selected) == 13:
+                    self.after(self.delay, lambda: self.delete_cards(cards))
+                    self.after(self.delay, self.pyramid_face_up)
                 else:
-                    self.after(self.delay, lambda: self.remove_pins(card1, card2))
-                self.opened = []
-       
+                    self.after(self.delay, lambda: self.remove_pins(cards))
+                self.selected = []
 
-    def remove_pins(self, *cards):
+ 
+
+    def remove_pins(self, cards):
         for card in cards:
             self.delete(card.pin)
             card.pin = None
-        self.update_status()
+       
 
-
-    def delete_cards(self, *cards):
+    def delete_cards(self, cards):
         for card in cards:
-            card.face.dele = True
+            card.dele = True
             self.delete(card.id)
-            self.delete(card.pin)
-        self.update_status()
-        self.after(self.delay, self.pyramid_face_up)
+        self.remove_pins(cards)
 
 
     def update_status(self, card=''):
         if not card:
             status = ''
         else:
-            val = card.status if card.status == 'jocker' else card.face.value
-            if val == 13 or not self.opened:
+            val = card.status if card.status == 'jocker' else card.value
+            if val == 13 or not self.selected:
                 status = val
-            elif len(self.opened) == 1:
+            elif len(self.selected) == 1:
                 try:
                     text = self.status_text.get()
                     status = '{} + {} = {}'.format(text, val, int(text) + int(val))
