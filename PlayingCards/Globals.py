5a1cda374c5066419e45dbc622c6ba276cf12127
taKana671 <kanae5321@gmail.com>
2021-07-25 22:52:40 +0900

    First commit

---
+++ PlayingCards/Globals.py
@@ -0,0 +1,28 @@
+APP_NAME = 'PlayCards'
+IMAGE_ROOT = 'images'
+CARD_ROOT = 'cards'
+
+CLOSE = 'Close'
+PYRAMID = 'Pyramid'
+CLOVER = 'FourLeafClover'
+KLONEDIKE = 'Klonedike'
+RULES = 'Rules'
+COUPLE = 'Couple'
+
+PIN = 'pin'
+BACK = 'back'
+RELOAD = 'reload'
+PAD = '0.75'
+
+BOARD_WIDTH = 1000
+BOARD_HEIGHT = 600
+BOARD_COLOR = 'green'
+MOVE_SPEED = 1
+PIN_OFFSET_X = 18
+PIN_OFFSET_y = 30
+
+DISAPPEAR = 'sounds/disappear.wav'
+LINEUP = 'sounds/lineup.wav'
+MISTAKE = 'sounds/mistake.wav'
+SHUFFLE = 'sounds/shuffle.wav'
+OPEN = 'sounds/open.wav'
